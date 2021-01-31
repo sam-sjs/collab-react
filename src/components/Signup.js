be989d8d68ba33468fd9ce6dd8ef62e67dedb171
Sam Skinner <sam.sjskinner@gmail.com>
2021-01-31 14:26:33 +1100

    Signup page setup and basic tag system working

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, {useState} from 'react'
+import api from '../lib/api'
+import './Signup.css'
+
 
 const Signup = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [tag, setTag] = useState('');
+  const [confirmedTags, setConfirmedTags] = useState([]);
+
+  const handleConfirmTag = (e) => {
+    setConfirmedTags([...confirmedTags, tag]);
+    setTag('');
+  }
+
+  const handleSubmit = () => {
+    api.postNewUser({
+      params: {
+        name: name,
+        email: email,
+        tags: confirmedTags
+      }
+    });
+  }
+
   return (
-    <div>
-      SIGNUP
+    <div className="signup">
+      <h2>Create new user:</h2>
+      <form id="newUser" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Enter name"
+          onChange={e => setName(e.target.value)}
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="text"
+          placeholder="Enter email"
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="tags">Add Tags:</label>
+        <input
+          id="tags"
+          type="text"
+          value={tag}
+          placeholder="Add some tags to your profile!"
+          onChange={e => setTag(e.target.value)}
+        />
+        <button onClick={handleConfirmTag}>Add Tag</button>
+        <div className="confirmedTags">
+          {
+            confirmedTags.map((t, i) => (
+              <span key={i}>{t}</span>
+            ))
+          }
+        </div>
+
+        <button form="newUser">Signup!</button>
+      </form>
     </div>
   );
 }
--- src/lib/api.js
+++ src/lib/api.js
@@ -4,6 +4,10 @@ import {config} from './constants'
 const api = {
   getProjects() {
     return axios.get(`${config.url.API_URL}/flights`);
+  },
+
+  postNewUser(params) {
+    return axios.post(`${config.url.API_URL}/users/create`, params);
   }
 };
 
